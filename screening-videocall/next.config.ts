e0ce5748e67a71df389131976f5a3f55e4d3579b
Rajaswa <rajaswaanand108@gmail.com>
2025-10-31 16:03:29 +0530

    Fix: add meeting route rewrite

--- screening-videocall/next.config.ts
+++ screening-videocall/next.config.ts
@@ -1,7 +1,22 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/meeting/:path*",
+        destination: "/meeting/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
