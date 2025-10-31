bc9621f0aed69fc8326fbd163714f2a9a5b80a8a
Rajaswa <rajaswaanand108@gmail.com>
2025-10-31 16:19:13 +0530

    Fix dynamic meeting route

--- screening-videocall/next.config.ts
+++ screening-videocall/next.config.ts
@@ -1,22 +1,7 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/meeting/:path*",
-        destination: "/meeting/:path*",
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
